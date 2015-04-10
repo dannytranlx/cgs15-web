a783179312416ed71a7b3c4f207eeeef6b99eb93
dannytranlx <danny.tran@radialpoint.com>
2015-04-09 21:31:36 -0400

    Update users table template

--- scripts/app/modules/users/views/users-view.js
+++ scripts/app/modules/users/views/users-view.js
@@ -3,6 +3,7 @@ define(function (require) {
   var Mn = require('marionette');
 
   var UserItemView = Mn.ItemView.extend({
+    tagName: 'tr',
     template: require('hbars!../templates/user-list-item-template')
   });
 
@@ -10,6 +11,6 @@ define(function (require) {
     className: 'container',
     template: require('hbars!../templates/users-list-template'),
     childView: UserItemView,
-    childViewContainer: 'ul'
+    childViewContainer: 'tbody'
   });
 });
